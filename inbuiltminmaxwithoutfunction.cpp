401fb2da4f45d2baecb0bd6cf7d43b48cf71b9c3
Atahar-Nur-Refat <ataharrifat@gmail.com>
2025-02-14 11:32:54 +0600

    Add files via upload

---
+++ inbuiltminmaxwithoutfunction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x,y;
+cin>> x>>y;
+int minimum=min(x,y);
+cout<<"the min is "<< minimum<<endl;
+int maximum=max(x,y);
+cout<<"the max is "<< maximum;
+
+
+    
+  return 0;
+}
