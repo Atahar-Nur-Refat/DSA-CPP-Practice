0d1f176f5be164f7e3bdbd02618d7de0a95dd84f
Atahar-Nur-Refat <ataharrifat@gmail.com>
2025-02-14 16:43:57 +0600

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void printname(string name ){
+    cout<<"Hello "<<name<<endl;
+}
+int main(){
+    string name,name2,name3,name4;
+
+
+printname(name);
+printname(name2);
+printname(name3);
+printname(name4);
+    return 0;
+}
